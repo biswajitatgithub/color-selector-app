f4b294bd78470cf0977e1848719c0a54a4bf67d7
Biswajit Sharma <biswajitsharmait@gmail.com>
2022-12-14 22:08:34 +0530

    Basic working product ready with color change with slider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,101 @@
-import './App.css';
+import React, { useState } from "react";
+import SliderInput from "./Components/Slider/SliderInput";
+import styled from "styled-components";
+import "./App.css";
+
+const ColorSelector = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(
+    ${(props) => props.redCode},
+    ${(props) => props.greenCode},
+    ${(props) => props.blueCode}
+  );
+  & .color-selector-panel {
+    width: 30%;
+    background-color: #ddd;
+    padding: 2rem;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    & .color {
+      display: grid;
+      grid-auto-flow: row;
+      grid-template-columns: 4fr 1fr;
+    }
+  }
+  & header {
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+  & .hex-color-code .selected-color {
+    width: 3rem;
+    height: 3rem;
+    background-color: rgb(
+      ${(props) => props.redCode},
+      ${(props) => props.greenCode},
+      ${(props) => props.blueCode}
+    );
+    border: 2px solid
+      rgb(
+        ${(props) => props.redCode - 77},
+        ${(props) => props.greenCode - 107},
+        ${(props) => props.blueCode - 222}
+      );
+  }
+`;
 
 function App() {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const onSliderChange = (color, value) => {
+    if (color === "red") setRed(value);
+    else if (color === "green") setGreen(value);
+    else if (color === "blue") setBlue(value);
+  };
   return (
-    <h1>Hi</h1>
+    <ColorSelector redCode={red} greenCode={green} blueCode={blue}>
+      <div className="color-selector-panel">
+        <header>
+          <h1>Select Color</h1>
+        </header>
+        <div className="color">
+          <div className="selector">
+            <SliderInput
+              onSliderChange={onSliderChange}
+              currentValue={red}
+              color={"red"}
+              name={"red"}
+              min={"0"}
+              max={"255"}
+            />
+            <SliderInput
+              onSliderChange={onSliderChange}
+              currentValue={green}
+              color={"green"}
+              name={"green"}
+              min={"0"}
+              max={"255"}
+            />
+            <SliderInput
+              onSliderChange={onSliderChange}
+              currentValue={blue}
+              color={"blue"}
+              name={"blue"}
+              min={"0"}
+              max={"255"}
+            />
+          </div>
+          <div className="hex-color-code">
+            <div className="selected-color"></div>
+            <div className="selected-hexcode"></div>
+          </div>
+        </div>
+      </div>
+    </ColorSelector>
   );
 }
 
---
+++ src/Components/Slider/SliderInput.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+const Slide = styled.div`
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items:center;
+  & .color-holder {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    margin-bottom: 0.5rem;
+  }
+  & .color {
+    width: 1.5rem;
+    height: 1.5rem;
+    background-color: ${(props) => props.color};
+    border-radius: 50%;
+    margin-right: 0.2rem;
+  }
+  & .range {
+    margin-bottom: 0.6rem;
+    width: 100%;
+    display: grid;
+    grid-auto-flow: row;
+    grid-template-columns: 1fr 4fr 1fr;
+    grid-column-gap: 0.5rem;
+    & .min {
+      justify-self: end;
+    }
+    & .max {
+      justify-self: start;
+    }
+  }
+`;
+
+const SliderInput = (props) => {
+  const sliderChangeHandler = (event) => {
+    props.onSliderChange(event.target.id, event.target.value);
+  };
+  return (
+    <Slide color={props.color}>
+      <div className="color-holder">
+        <div className="color"></div>
+        <span>{props.currentValue}</span>
+      </div>
+      <div className="range">
+        <div className="min">{props.min}</div>
+        <input
+          type="range"
+          name={props.name}
+          id={props.name}
+          min={props.min}
+          max={props.max}
+          onChange={sliderChangeHandler}
+        />
+        <span className="max">{props.min}</span>
+      </div>
+    </Slide>
+  );
+};
+
+export default SliderInput;
