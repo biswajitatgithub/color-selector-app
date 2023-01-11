66c0294057b555757ff38a64a4e1914a058183b8
Biswajit Sharma <biswajitsharmait@gmail.com>
2023-01-11 22:21:21 +0530

    add hex code copy functionality

--- src/App.js
+++ src/App.js
@@ -29,6 +29,12 @@ const ColorSelector = styled.div`
     margin-bottom: 2rem;
     text-align: center;
   }
+  & .hex-color-code{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  }
   & .hex-color-code .selected-color {
     width: 3rem;
     height: 3rem;
@@ -44,17 +50,40 @@ const ColorSelector = styled.div`
         ${(props) => props.blueCode - 222}
       );
   }
+  & .hex-copy{
+    padding: .5rem 1rem;
+    background-color: #000;
+    color:white;
+    cursor: pointer;
+  }
 `;
 
 function App() {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+  const [copy, setCopy] = useState(false);
+
+  let redHex = Number(red).toString(16);
+  let greenHex = Number(green).toString(16);
+  let blueHex = Number(blue).toString(16);
+
+  redHex = redHex.length === 1 ? "0" + redHex : redHex;
+  greenHex = greenHex.length === 1 ? "0" + greenHex : greenHex;
+  blueHex = blueHex.length === 1 ? "0" + blueHex : blueHex;
+ 
+  let hexString = ("#"+redHex+greenHex+blueHex).toUpperCase();
 
   const onSliderChange = (color, value) => {
     if (color === "red") setRed(value);
     else if (color === "green") setGreen(value);
     else if (color === "blue") setBlue(value);
+    setCopy(false);
+  };
+  const copyOnClickHandler = (event) => {
+    event.preventDefault();
+    navigator.clipboard.writeText(hexString);
+    setCopy(true);
   };
   return (
     <ColorSelector redCode={red} greenCode={green} blueCode={blue}>
@@ -91,7 +120,10 @@ function App() {
           </div>
           <div className="hex-color-code">
             <div className="selected-color"></div>
-            <div className="selected-hexcode"></div>
+            <div className="selected-hexcode">{hexString}</div>
+            <button onClick={copyOnClickHandler} className="hex-copy">
+              {copy ? "Copied!" : "Copy"}
+            </button>
           </div>
         </div>
       </div>
